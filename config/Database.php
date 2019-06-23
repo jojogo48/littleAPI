c7c4ea1426cd653044c04eddcc7e1bd7e52f9d91
jojogo48 <jojogo48@gmail.com>
2019-06-23 09:00:54 +0800

    creat Database Post

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+    class Database{
+        //DB parameters
+        private $host='localhost';
+        private $db_name='php_rest';
+        private $username='root';
+        private $password='';
+        private $conn;
+        //DB connction
+        public function connet(){
+            $this->conn=null;
+
+            try{
+                $this->conn = new PDO('mysql:host= '.$this->$host.';dbname='.$this->$db_name,
+                $this->username,$this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            }catch(PDOException $e){
+                echo 'connection error'.$e->getMessagee();
+            }
+            return $this->conn;
+        }
+    }
